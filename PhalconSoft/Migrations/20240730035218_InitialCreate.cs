475adfbdef53595ed7c2945c3eb6b6def9cd82c6
yunusemreerken <ynsmrrkn@gmail.com>
2024-07-30 06:54:10 +0300

    projeye devam

---
+++ PhalconSoft/Migrations/20240730035218_InitialCreate.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhalconSoft.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsletterSubscribers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsletterSubscribers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    SeoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeoDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PortfolioTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PortfolioDetail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryNavigationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Portfolios_Categories_CategoryNavigationId",
+                        column: x => x.CategoryNavigationId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CoverPhoto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ThumbnailPhoto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Portfolio = table.Column<int>(type: "int", nullable: false),
+                    PortfolioNavigationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PortfolioPhotos_Portfolios_PortfolioNavigationId",
+                        column: x => x.PortfolioNavigationId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioPhotos_PortfolioNavigationId",
+                table: "PortfolioPhotos",
+                column: "PortfolioNavigationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolios_CategoryNavigationId",
+                table: "Portfolios",
+                column: "CategoryNavigationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsletterSubscribers");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioPhotos");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
